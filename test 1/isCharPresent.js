61f9bc93d52e7623257628fa4beb23a303177406
Lyudmila Pikulina <38284734+milasorokina96@users.noreply.github.com>
2020-12-07 01:23:21 +0200

    Update isCharPresent.js

--- test 1/isCharPresent.js
+++ test 1/isCharPresent.js
@@ -1,12 +1,10 @@
 module.exports = isCharPresent;
 
 function isCharPresent(str, sym) {    
-    let arrOfStr = str.split('');
-    for(let i = 0; i < arrOfStr.length; i++){
-        if (arrOfStr[i] === sym) {
+    for(let i = 0; i < str.length; i++){
+        if (str[i] === sym) {
             return true;
-        } else {
-            return false;
         }
     }
+    return false;
 }
